958b6bd409713d2883d23f30a9b14cad61df4426
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2024-07-08 18:18:28 -0300

    add cursor point na ordenação na pagina de santuarios

--- src/components/SanctuariesTable.tsx
+++ src/components/SanctuariesTable.tsx
@@ -28,84 +28,88 @@ export default function SanctuariesTable({
   setSortField,
   sortDirection,
   setSortDirection,
-  isLoading
+  isLoading,
 }: SanctuaryTableProps) {
-
   const [idSortState, setIdSortState] = useState<SortOrderState>("none");
   const [idSortStateIcon, setIdSortStateIcon] = useState(SortOrderNoneIcon);
 
   const [nameSortState, setNameSortState] = useState<SortOrderState>("none");
   const [nameSortStateIcon, setNameSortStateIcon] = useState(SortOrderNoneIcon);
 
-  const [countrySortState, setCountrySortState] = useState<SortOrderState>("none");
-  const [countrySortStateIcon, setCountrySortStateIcon] = useState(SortOrderNoneIcon);
+  const [countrySortState, setCountrySortState] =
+    useState<SortOrderState>("none");
+  const [countrySortStateIcon, setCountrySortStateIcon] =
+    useState(SortOrderNoneIcon);
 
   const [stateSortState, setStateSortState] = useState<SortOrderState>("none");
-  const [stateSortStateIcon, setStateSortStateIcon] = useState(SortOrderNoneIcon);
+  const [stateSortStateIcon, setStateSortStateIcon] =
+    useState(SortOrderNoneIcon);
 
   const [citySortState, setCitySortState] = useState<SortOrderState>("none");
   const [citySortStateIcon, setCitySortStateIcon] = useState(SortOrderNoneIcon);
 
-  const [addressSortState, setAddressSortState] = useState<SortOrderState>("none");
-  const [addressSortStateIcon, setAddressSortStateIcon] = useState(SortOrderNoneIcon);
+  const [addressSortState, setAddressSortState] =
+    useState<SortOrderState>("none");
+  const [addressSortStateIcon, setAddressSortStateIcon] =
+    useState(SortOrderNoneIcon);
 
   const [ownerSortState, setOwnerSortState] = useState<SortOrderState>("none");
-  const [ownerSortStateIcon, setOwnerSortStateIcon] = useState(SortOrderNoneIcon);
-
+  const [ownerSortStateIcon, setOwnerSortStateIcon] =
+    useState(SortOrderNoneIcon);
 
   type FieldStatesMap = {
     [key: string]: {
-      state: SortOrderState,
-      setState: React.Dispatch<React.SetStateAction<SortOrderState>>,
-      icon: string,
-      setStateIcon: React.Dispatch<React.SetStateAction<string>>
-    }
-  }
+      state: SortOrderState;
+      setState: React.Dispatch<React.SetStateAction<SortOrderState>>;
+      icon: string;
+      setStateIcon: React.Dispatch<React.SetStateAction<string>>;
+    };
+  };
 
   const fieldsStates: FieldStatesMap = {
-    "id": { 
-      state: idSortState, 
+    id: {
+      state: idSortState,
       setState: setIdSortState,
       icon: idSortStateIcon,
-      setStateIcon: setIdSortStateIcon 
+      setStateIcon: setIdSortStateIcon,
     },
-    "name": { 
-      state: nameSortState, 
+    name: {
+      state: nameSortState,
       setState: setNameSortState,
       icon: nameSortStateIcon,
-      setStateIcon: setNameSortStateIcon
+      setStateIcon: setNameSortStateIcon,
     },
-    "country": { 
-      state: countrySortState, 
+    country: {
+      state: countrySortState,
       setState: setCountrySortState,
       icon: countrySortStateIcon,
-      setStateIcon: setCountrySortStateIcon
+      setStateIcon: setCountrySortStateIcon,
     },
-    "state": { 
+    state: {
       state: stateSortState,
       setState: setStateSortState,
       icon: stateSortStateIcon,
-      setStateIcon: setStateSortStateIcon
+      setStateIcon: setStateSortStateIcon,
     },
-    "city": { 
+    city: {
       state: citySortState,
       setState: setCitySortState,
       icon: citySortStateIcon,
-      setStateIcon: setCitySortStateIcon 
+      setStateIcon: setCitySortStateIcon,
     },
-    "address": { 
+    address: {
       state: addressSortState,
       setState: setAddressSortState,
       icon: addressSortStateIcon,
-      setStateIcon: setAddressSortStateIcon 
+      setStateIcon: setAddressSortStateIcon,
     },
-    "owner": { 
+    owner: {
       state: ownerSortState,
       setState: setOwnerSortState,
       icon: ownerSortStateIcon,
-      setStateIcon: setOwnerSortStateIcon 
+      setStateIcon: setOwnerSortStateIcon,
     },
-  }
+  };
 
   const handleSortState = (selectedKey: string) => {
     Object.keys(fieldsStates).forEach((key) => {
@@ -127,31 +131,83 @@ export default function SanctuariesTable({
 
     setSortField(selectedKey);
     setSortDirection(newSortDirection);
-  }
-
-  // const getSortOrderIcon = (state: SortOrderState) => {
-  //   if (state === "up") return SortOrderUpIcon;
-  //   if (state === "down") return SortOrderDownIcon;
-  //   return SortOrderNoneIcon;
-  // }
+  };
 
   useEffect(() => {
     if (!isLoading) {
-      fieldsStates[sortField].setStateIcon(sortDirection === "asc" ? SortOrderUpIcon : SortOrderDownIcon);
+      fieldsStates[sortField].setStateIcon(
+        sortDirection === "asc" ? SortOrderUpIcon : SortOrderDownIcon
+      );
     }
-  }, [sanctuaries, isLoading])
+  }, [sanctuaries, isLoading]);
 
   return (
     <table>
       <thead>
         <tr>
-          <th className="align-middle text-center">id <img src={idSortStateIcon} alt="Sort-icon" onClick={() => handleSortState("id")} /> </th>
-          <th className="align-middle text-center">name <img src={nameSortStateIcon} alt="Sort-icon" onClick={() => handleSortState("name")} /> </th>
-          <th className="align-middle text-center">country <img src={countrySortStateIcon} alt="Sort-icon" onClick={() => handleSortState("country")} /> </th>
-          <th className="align-middle text-center">state <img src={stateSortStateIcon} alt="Sort-icon" onClick={() => handleSortState("state")} /> </th>
-          <th className="align-middle text-center">city <img src={citySortStateIcon} alt="Sort-icon" onClick={() => handleSortState("city")} /> </th>
-          <th className="align-middle text-center">address <img src={addressSortStateIcon} alt="Sort-icon" onClick={() => handleSortState("address")} /> </th>
-          <th className="align-middle text-center">owner <img src={ownerSortStateIcon} alt="Sort-icon" onClick={() => handleSortState("owner")} /> </th>
+          <th className="align-middle text-center">
+            id{" "}
+            <img
+              src={idSortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("id")}
+            />{" "}
+          </th>
+          <th className="align-middle text-center">
+            name{" "}
+            <img
+              src={nameSortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("name")}
+            />{" "}
+          </th>
+          <th className="align-middle text-center">
+            country{" "}
+            <img
+              src={countrySortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("country")}
+            />{" "}
+          </th>
+          <th className="align-middle text-center">
+            state{" "}
+            <img
+              src={stateSortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("state")}
+            />{" "}
+          </th>
+          <th className="align-middle text-center">
+            city{" "}
+            <img
+              src={citySortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("city")}
+            />{" "}
+          </th>
+          <th className="align-middle text-center">
+            address{" "}
+            <img
+              src={addressSortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("address")}
+            />{" "}
+          </th>
+          <th className="align-middle text-center">
+            owner{" "}
+            <img
+              src={ownerSortStateIcon}
+              alt="Sort-icon"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleSortState("owner")}
+            />{" "}
+          </th>
           <th className="align-middle text-center">actions</th>
         </tr>
       </thead>
