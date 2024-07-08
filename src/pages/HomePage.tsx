74fd11d2bcb69c6da870c76caabb5698cf5e0b76
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2024-07-08 18:11:06 -0300

    add mudanças de estilo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,14 +5,14 @@ export default function Home() {
                 <h1 className="text-center mb-4">Quem somos</h1>
                 <div className="row">
                     <div className="col-md-6">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nemo velit obcaecati, dolores beatae molestias eius maiores soluta voluptatum? Distinctio deleniti esse expedita porro voluptates, quia alias illo iusto culpa?
+                        <p style={{textAlign: "justify"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nemo velit obcaecati, dolores beatae molestias eius maiores soluta voluptatum? Distinctio deleniti esse expedita porro voluptates, quia alias illo iusto culpa?
                         Nostrum quis doloremque maiores officia voluptatibus blanditiis aperiam enim, recusandae nesciunt? Dolor nisi similique maiores doloribus itaque dolores illo quibusdam inventore provident? Voluptate impedit minus ex modi soluta necessitatibus officia.
                         Atque quae nostrum rem repellendus cumque beatae. Tempora inventore assumenda ad accusantium enim beatae aliquid, velit laudantium repellat sed voluptas aliquam, illo optio. Impedit iusto facere amet nisi reiciendis pariatur?
                         Sint dignissimos animi accusamus, illo accusantium consectetur eveniet labore inventore error, consequatur velit cumque fugiat quia pariatur reprehenderit totam! Quas soluta odit saepe enim vitae voluptates officia totam corrupti! Eum.
                         Rerum, fugit amet itaque sequi dolore assumenda eveniet repellendus deleniti eaque molestias fugiat? Praesentium perspiciatis, consequatur error ipsa officiis quo deserunt? Cupiditate rem, eius explicabo provident eaque iusto voluptatibus impedit.</p>
                     </div>
                     <div className="col-md-6">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nemo velit obcaecati, dolores beatae molestias eius maiores soluta voluptatum? Distinctio deleniti esse expedita porro voluptates, quia alias illo iusto culpa?
+                        <p style={{textAlign: "justify"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nemo velit obcaecati, dolores beatae molestias eius maiores soluta voluptatum? Distinctio deleniti esse expedita porro voluptates, quia alias illo iusto culpa?
                         Nostrum quis doloremque maiores officia voluptatibus blanditiis aperiam enim, recusandae nesciunt? Dolor nisi similique maiores doloribus itaque dolores illo quibusdam inventore provident? Voluptate impedit minus ex modi soluta necessitatibus officia.
                         Atque quae nostrum rem repellendus cumque beatae. Tempora inventore assumenda ad accusantium enim beatae aliquid, velit laudantium repellat sed voluptas aliquam, illo optio. Impedit iusto facere amet nisi reiciendis pariatur?
                         Sint dignissimos animi accusamus, illo accusantium consectetur eveniet labore inventore error, consequatur velit cumque fugiat quia pariatur reprehenderit totam! Quas soluta odit saepe enim vitae voluptates officia totam corrupti! Eum.
@@ -23,7 +23,7 @@ export default function Home() {
 
             <div className="container my-5">
                 <h1 className="text-center mb-4">Nossa missão</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus odio optio dolorum, illum incidunt cum iste iusto dolore ducimus ut tenetur iure commodi voluptas, nisi repudiandae omnis obcaecati sint.
+                <p style={{textAlign: "justify"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus odio optio dolorum, illum incidunt cum iste iusto dolore ducimus ut tenetur iure commodi voluptas, nisi repudiandae omnis obcaecati sint.
                 Ut laboriosam consequuntur illo ipsam assumenda optio. Asperiores commodi aliquid facilis corporis, quisquam obcaecati quia eligendi vel facere quibusdam? Culpa molestiae dolor porro tenetur repellat in ea maiores fugit fuga.
                 Similique voluptatem facere corrupti ratione repudiandae consectetur quae, deleniti iusto velit assumenda corporis dolorum debitis fuga sunt architecto esse alias eligendi facilis perferendis unde harum sapiente veritatis animi. Nemo, officia.
                 Facere pariatur magni delectus magnam esse tenetur et mollitia omnis tempore est beatae, iusto eveniet aliquid id laboriosam sunt consectetur consequatur amet aliquam. Expedita blanditiis voluptas quam adipisci autem quasi.
